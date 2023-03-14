b419cff56c3990fa11f39b293592fc2b68c89093
Giorgi Abashidze <126471823+giorgi1121@users.noreply.github.com>
2023-03-14 19:15:45 +0400

    Create extensions.py

---
+++ extensions.py
@@ -0,0 +1,20 @@
+#create variable
+file = input("Enter file name: ").lower().strip()
+
+#make conditionals
+if "gif" in file:
+    print("image/gif")
+elif "jpg" in file:
+    print("image/jpeg")
+elif "jpeg" in file:
+    print("image/jpeg")
+elif "png" in file:
+    print("image/png")
+elif "pdf" in file:
+    print("application/pdf")
+elif "txt" in file:
+    print("text/plain")
+elif "zip" in file:
+    print("application/zip")
+else:
+    print("application/octet-stream")
